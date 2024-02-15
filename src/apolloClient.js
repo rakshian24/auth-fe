5ecb844998d5dae9af3216af49b5f2054ae9de9f
Rakshith Sindhiya R <rakshithsindhiyar@Rakshiths-MacBook-Pro.local>
2024-02-15 15:42:14 +0530

    feat: added auth context and provider

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,9 +1,21 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { SERVER_ENDPOINT_URI } from "./constants";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { APP_TOKEN_KEY, SERVER_ENDPOINT_URI } from "./constants";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({ uri: SERVER_ENDPOINT_URI });
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem(APP_TOKEN_KEY) || "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: SERVER_ENDPOINT_URI,
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
 
 export default client;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,2 @@
 export const SERVER_ENDPOINT_URI = `https://rakshian-auth-be.vercel.app/graphql`;
+export const APP_TOKEN_KEY = "rakshAppToken";
